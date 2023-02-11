4d3ad12a3773524a8846394b04844ceba1c6ac6a
MAHESH MUNGASE <98934559+Mahesh2357@users.noreply.github.com>
2023-02-11 21:50:23 +0530

    Add files via upload
    Task-3 of my Oasis Infobyte Java Development internship.

---
+++ ATM.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class ATM extends TestMain {
+    public static void main(String[] args) throws IOException {
+        TestMain optionMenu = new TestMain();
+
+        optionMenu.getLogin();
+    }
+}
